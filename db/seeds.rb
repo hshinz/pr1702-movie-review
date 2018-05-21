31f83ec1d82c09d4089e1c562d9c1d5f8603c18d
giangmt <maitruonggiangbkhn@gmail.com>
2018-05-21 18:21:48 +0700

    play movie

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,50 +1,51 @@
-Genre.create!(name: "Action")
-Genre.create!(name: "Adventure")
-Description.create!(director_name: "Luc Besson",
-  cast_name: "Rosanna Arquette")
-Description.create!(director_name: "Steven S. DeKnight",
-  cast_name: "John Boyega")
-Movie.create!(name: "Pacific Rim: Uprising",
-  image: "https://yts.am/assets/images/movies/pacific_rim_uprising_2018/medium-cover.jpg",
-  trailer: "https://www.youtube.com/watch?v=_EhiLLOhVis",
-  synopsis: "Jake Pentecost, son of Stacker Pentecost, reunites with Mako Mori to lead a new generation of Jaeger pilots, including rival Lambert and 15-year-old hacker Amara, against a new Kaiju threat.",
-  release_date: "2018-04-15",
-  genre_id: 1,
-  description_id: 1)
-Movie.create!(name: "The Big Blue",
-  image: "https://yts.am/assets/images/movies/the_big_blue_1988/medium-cover.jpg",
-  trailer: "https://www.youtube.com/watch?v=82onGmBx9ZM",
-  synopsis: "Enzo and Jacques have known each other for a long time. Their friendship started in their childhood days in the Mediterranean. They were not real friends in these days, but there was something they both loved and used to do the whole day long: diving. One day Jacques' father, who was a diver too, died in the Mediterranean sea. After that incident Enzo and Jacques lost contact. After several years, Enzo and Jacques had grown up, Johanna, a young clerk in an insurance office, has to go to Peru. There she meets Jacques who is being studied by a group of scientists. He dives for some minutes into ice-cold water and the scientists monitor his physical state that is more like a dolphin than human. Johanna can not believe what she sees and gets very interested in Jacques but she's unable to get acquainted with him. Some weeks later back in her office, she finds out that Jacques will be competing in a diving championship that takes place in Taormina, Sicily. In order to see Jacques again she ...",
-  release_date: "1998-09-12",
-  genre_id: 2,
-  description_id: 2)
-Movie.create!(name: "Black Panther",
-  image: "https://yts.am/assets/images/movies/black_panther_2018/medium-cover.jpg",
-  trailer: "https://www.youtube.com/watch?v=xjDjIWPwcPU",
-  synopsis: "After the events of Captain America: Civil War, King T'Challa returns home to the reclusive, technologically advanced African nation of Wakanda to serve as his country's new leader. However, T'Challa soon finds that he is challenged for the throne from factions within his own country. When two foes conspire to destroy Wakanda, the hero known as Black Panther must team up with C.I.A. agent Everett K. Ross and members of the Dora Milaje, Wakandan special forces, to prevent Wakanda from being dragged into a world war.",
-  release_date: "1998-09-12",
-  genre_id: 2,
-  description_id: 2)
-Movie.create!(name: "Swinging Safari",
-  image: "https://yts.am/assets/images/movies/swinging_safari_2018/medium-cover.jpg",
-  trailer: "https://www.youtube.com/watch?v=82onGmBx9ZM",
-  synopsis: "Australia, 1975. The beach suburb of Nobbys Beach is a place that revolves around surf mats, baby oil, boxed wine and the new miracle of Kentucky Fried Chicken. 14 year-old Jeff tries to find his feet in a world changing faster than his hormones, and deal with his crush on shy and sensitive girl-next-door Melly. When the beach town suddenly hits the spotlight after the body of a 200-ton whale is washed ashore, Jeff and Melly think it's the biggest thing that ever happened in their lives. Meanwhile, their eccentric parents are catching up with the sexual revolution that has also washed up on Australia's beaches. And just like the decaying whale, it's all about to go spectacularly wrong.",
-  release_date: "2018-10-12",
-  genre_id: 2,
-  description_id: 2)
-Movie.create!(name: "Dark Crimes",
-  image: "https://yts.am/assets/images/movies/dark_crimes_2016/medium-cover.jpg",
-  trailer: "https://www.youtube.com/watch?v=82onGmBx9ZM",
-  synopsis: "A murder investigation of a slain business man turns to clues found in an author's book about an eerily similar crime. Based on the 2008 article by David Grann.",
-  release_date: "1998-09-12",
-  genre_id: 2,
-  description_id: 2)
-Movie.create!(name: "The Dark Knight",
-  image: "https://yts.am/assets/images/movies/The_Dark_Knight_2008/medium-cover.jpg",
-  trailer: "https://www.youtube.com/watch?v=82onGmBx9ZM",
-  synopsis: "Set within a year after the events of Batman Begins, Batman, Lieutenant James Gordon, and new district attorney Harvey Dent successfully begin to round up the criminals that plague Gotham City until a mysterious and sadistic criminal mastermind known only as the Joker appears in Gotham, creating a new wave of chaos. Batman's struggle against the Joker becomes deeply personal, forcing him to and improve his technology to stop him. A love triangle develops between Bruce Wayne, Dent and Rachel Dawes.",
-  release_date: "1998-09-12",
-  genre_id: 2,
-  description_id: 2)
-Movie.update([2,3],
-  [{length: "01:00:00"},{length: "02:21:22"}] )
+# Genre.create!(name: "Action")
+# Genre.create!(name: "Adventure")
+# Description.create!(director_name: "Luc Besson",
+#   cast_name: "Rosanna Arquette")
+# Description.create!(director_name: "Steven S. DeKnight",
+#   cast_name: "John Boyega")
+# Movie.create!(name: "Pacific Rim: Uprising",
+#   image: "https://yts.am/assets/images/movies/pacific_rim_uprising_2018/medium-cover.jpg",
+#   trailer: "https://www.youtube.com/watch?v=_EhiLLOhVis",
+#   synopsis: "Jake Pentecost, son of Stacker Pentecost, reunites with Mako Mori to lead a new generation of Jaeger pilots, including rival Lambert and 15-year-old hacker Amara, against a new Kaiju threat.",
+#   release_date: "2018-04-15",
+#   genre_id: 1,
+#   description_id: 1)
+# Movie.create!(name: "The Big Blue",
+#   image: "https://yts.am/assets/images/movies/the_big_blue_1988/medium-cover.jpg",
+#   trailer: "https://www.youtube.com/watch?v=82onGmBx9ZM",
+#   synopsis: "Enzo and Jacques have known each other for a long time. Their friendship started in their childhood days in the Mediterranean. They were not real friends in these days, but there was something they both loved and used to do the whole day long: diving. One day Jacques' father, who was a diver too, died in the Mediterranean sea. After that incident Enzo and Jacques lost contact. After several years, Enzo and Jacques had grown up, Johanna, a young clerk in an insurance office, has to go to Peru. There she meets Jacques who is being studied by a group of scientists. He dives for some minutes into ice-cold water and the scientists monitor his physical state that is more like a dolphin than human. Johanna can not believe what she sees and gets very interested in Jacques but she's unable to get acquainted with him. Some weeks later back in her office, she finds out that Jacques will be competing in a diving championship that takes place in Taormina, Sicily. In order to see Jacques again she ...",
+#   release_date: "1998-09-12",
+#   genre_id: 2,
+#   description_id: 2)
+# Movie.create!(name: "Black Panther",
+#   image: "https://yts.am/assets/images/movies/black_panther_2018/medium-cover.jpg",
+#   trailer: "https://www.youtube.com/watch?v=xjDjIWPwcPU",
+#   synopsis: "After the events of Captain America: Civil War, King T'Challa returns home to the reclusive, technologically advanced African nation of Wakanda to serve as his country's new leader. However, T'Challa soon finds that he is challenged for the throne from factions within his own country. When two foes conspire to destroy Wakanda, the hero known as Black Panther must team up with C.I.A. agent Everett K. Ross and members of the Dora Milaje, Wakandan special forces, to prevent Wakanda from being dragged into a world war.",
+#   release_date: "1998-09-12",
+#   genre_id: 2,
+#   description_id: 2)
+# Movie.create!(name: "Swinging Safari",
+#   image: "https://yts.am/assets/images/movies/swinging_safari_2018/medium-cover.jpg",
+#   trailer: "https://www.youtube.com/watch?v=82onGmBx9ZM",
+#   synopsis: "Australia, 1975. The beach suburb of Nobbys Beach is a place that revolves around surf mats, baby oil, boxed wine and the new miracle of Kentucky Fried Chicken. 14 year-old Jeff tries to find his feet in a world changing faster than his hormones, and deal with his crush on shy and sensitive girl-next-door Melly. When the beach town suddenly hits the spotlight after the body of a 200-ton whale is washed ashore, Jeff and Melly think it's the biggest thing that ever happened in their lives. Meanwhile, their eccentric parents are catching up with the sexual revolution that has also washed up on Australia's beaches. And just like the decaying whale, it's all about to go spectacularly wrong.",
+#   release_date: "2018-10-12",
+#   genre_id: 2,
+#   description_id: 2)
+# Movie.create!(name: "Dark Crimes",
+#   image: "https://yts.am/assets/images/movies/dark_crimes_2016/medium-cover.jpg",
+#   trailer: "https://www.youtube.com/watch?v=82onGmBx9ZM",
+#   synopsis: "A murder investigation of a slain business man turns to clues found in an author's book about an eerily similar crime. Based on the 2008 article by David Grann.",
+#   release_date: "1998-09-12",
+#   genre_id: 2,
+#   description_id: 2)
+# Movie.create!(name: "The Dark Knight",
+#   image: "https://yts.am/assets/images/movies/The_Dark_Knight_2008/medium-cover.jpg",
+#   trailer: "https://www.youtube.com/watch?v=82onGmBx9ZM",
+#   synopsis: "Set within a year after the events of Batman Begins, Batman, Lieutenant James Gordon, and new district attorney Harvey Dent successfully begin to round up the criminals that plague Gotham City until a mysterious and sadistic criminal mastermind known only as the Joker appears in Gotham, creating a new wave of chaos. Batman's struggle against the Joker becomes deeply personal, forcing him to and improve his technology to stop him. A love triangle develops between Bruce Wayne, Dent and Rachel Dawes.",
+#   release_date: "1998-09-12",
+#   genre_id: 2,
+#   description_id: 2)
+# Movie.update([2,3],
+#   [{length: "01:00:00"},{length: "02:21:22"}] )
+Movie.update([1,2],[{trailer: "_EhiLLOhVis"}, {trailer: "82onGmBx9ZM"}])
