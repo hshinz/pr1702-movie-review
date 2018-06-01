7bcd440b250e07fa6a9414967846d1bab209539c
hshinz <whyisme110222@gmail.com>
2018-06-01 19:11:18 +0700

    view user detail

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root "static_page#home"
 
   devise_for :users
+  resources :users do
+    resources :comments
+    resources :reviews
+  end
   resources :movies do
     resources :comments
     resources :reviews
